9d5d64216d3bcc3265e846f3f9551f4a7fa0d5bd
michelecatani <catani@uwindsor.ca>
2022-04-28 19:34:57 -0400

    new video

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content=""/>
+        <meta name="author" content="" />
+        <title>MCCOMP</title>
+        <link rel="icon" type="image/x-icon" href="assets/logoRounded.ico"/>
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">MCCOMP</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#about">About me</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#resume">Resume</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#aerial">Aerial Videography</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#leetcode">LeetCode Tutorials</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Socials</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" class="nav-link" target="_blank" rel="noreferrer noopener" href="https://ca.linkedin.com/in/michele-catani-294136134">LinkedIn</a>
+                                <a class="dropdown-item" class="nav-link" target="_blank" rel="noreferrer noopener" href="https://studio.youtube.com/channel/UCXF6KQUCbUh4cjcdUc2bjbQ">Youtube</a>
+                                <a class="dropdown-item" class="nav-link" target="_blank" rel="noreferrer noopener" href="https://www.instagram.com/mcatani7/?hl=en">Instagram</a>
+                                <a class="dropdown-item" class="nav-link" target="_blank" rel="noreferrer noopener" href="https://github.com/michelecatani">Github</a>
+                                <a class="dropdown-item" class="nav-link" target="_blank" rel="noreferrer noopener" href="https://leetcode.com/catanim/">LeetCode</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <!-- get this to work so it doesnt move welcome out of the way TODO
+                <div class ="small-troll-text">
+                    <h6 class ="text-white-50 mb-troll"> i liek space 2 lulz</h6>
+                </div>
+            -->
+                <div class="d-flex justify-content-center">
+                    
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">WELCOME</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">My name is Michele Catani. I'm a CS student at the University of Windsor as well as a budding drone pilot.  Thanks for checking out my website!</h2>
+                        <a class="btn btn-primary" href="#about">Let's go!</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-10">
+                        <h2 class="text-white mb-4">About me</h2>
+                        <p class="text-white-50">
+                            I started programming as a hobbyist in 2017, but I had different career ideas during those times, so I was never able to take it seriously.  However, I soon realized that I wanted to spend my professional life programming, and so I changed course to do so.
+                            <br><br>
+                            I enrolled in the Honours Applied Computing program at the University of Windsor in May of 2021, and since then I've been focusing a lot more of my energy to programming.  Recently, I started a Youtube channel where I post solutions to LeetCode problems (as well as drone footage), so feel free to check that out! Just navigate to the "Socials" dropdown menu (it may be under the three little lines at the top right if you're on a mobile device) to see links to all my social network accounts.
+                            <br><br>
+                            This website was built using a Bootstrap template, various scripts for the navbar and mobile responsiveness, asynchronous JS to fetch my projects from Github's API, and a stylesheet.  I hope you like it, and thanks again for taking the time to come here.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!---<p id="demo">Hello bob</p> this was a little js demo
+        <button onclick="hello()"> try it </button>--->
+        <!-- Projects-->
+        <section class = "thePT-section text-center" id="resume">
+            <div class="container px-5 px-lg-1">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-10">
+                        <h2 class="text-black-100 mb-4">Resume</h2>
+                        <h3 class="resumeListHeader">Skills</h3>
+                            <ul>
+                                <li class="resumeListItem">C and C++ (learned in school)</li>
+                                <li class="resumeListItem">Java programming (learned at school, used to solve LeetCode problems)</li>
+                                <li class="resumeListItem">Python (learned by myself, used to solve LeetCode problems and for my fourth year project)</li>
+                                <li class="resumeListItem">Android Studio Development (learned at school, used to build several mobile apps for course 4200)</li>
+                                <li class="resumeListItem">SQL (used to solve LeetCode problems and to help build a mock hospital database for my DBMS course)</li>
+                                <li class="resumeListItem">Web Development in HTML, CSS, PHP, and Javascript (used to build this website and to build the frontend of a mock hospital database project for my DBMS course)</li>
+                            </ul>
+                        <h3 class="resumeListHeader">Education</h3>
+                            <ul>
+                                <li class="resumeListItem">University of Windsor - Bachelor of Computing Science, May 2021-present</li>
+                                <li class="resumeListItem">University of Windsor - Double Major, Political Science and Economics, 2015-2019, BA</li>
+                            </ul>
+                        <h3 class="resumeListHeader">Work Experience</h3>
+                            <ul>
+                                <li class="resumeListItem">Software Developer, Google Developer Student Clubs, University of Windsor - 2022 - present</li>
+                                <li class="resumeListItem">Customer Experience Associate, TD Canada Trust - 2016 - present</li>
+                                <li class="resumeListItem">Caseworker/Intake Support Worker, Community Legal Aid - 2020</li>
+                            </ul>
+                        <h3 class="resumeListHeader">Hobbies/Interest</h3>
+                            <ul>
+                                <li class="resumeListItem">Aquariums</li>
+                                <li class="resumeListItem">Brazilian Jiu Jitsu and weightlifting</li>
+                                <li class="resumeListItem">Aerial Videography</li>
+                            </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class = "theVideo-section text-center" id="aerial">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-10">
+                        <h2 class="text-white-50 mb-4">Aerial Videography</h2>
+                            <p class="text-white-50">
+                                I bought my first drone in April of 2022. I want to hone my skills to the point where my clips tell a story and move the audience on an emotional level.  Check out my most recent videos below, or you can click <a href= "https://www.youtube.com/playlist?list=PLa0e8IzKsZnwWNICBCVY134fmiy1M9_1Z" target="_blank" rel="noreferrer noopener">this link</a> to go to the YouTube playlist!    
+                            </p>
+                            <div  id="aerialButton">
+                                <button onClick="showAerialVideos()" class="btn btn-primary">See Videos</button>
+                            </div>
+                            <div id="aerialVideos">
+
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class = "thePT-section text-center" id="leetcode">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-10">
+                        <h2 class="text-black-100 mb-4">LeetCode tutorials</h2>
+                            <p class="text-black-100">
+                            I first started grinding LeetCode in December 2021.  I found it a great way to implement a lot of the more abstract data structures that we mentioned in passing in the Data Structures and Algorithms course, and since February 2022 I've been posting video explanations to Youtube.  Check out my most recent videos below, or you can click <a href= "https://www.youtube.com/playlist?list=PLa0e8IzKsZnwiAEQTuDxQp_l8NyHfM_Q7" target="_blank" rel="noreferrer noopener">this link</a> to go to the YouTube playlist!
+                            </p>
+                            <div  id="leetCodeButton">
+                                <button onClick="showLeetCodeVideos()" class="btn btn-primary">See Videos</button>
+                            </div>
+                            <div id="leetCodeVideos">
+                            
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class = "theVideo-section text-center" id="projects">
+            <div class="container px-4 px-lg-0">
+                <div class="row gx-4 gx-lg-1 justify-content-center">
+                    <div class="col-lg-12">
+                        <h2 class="text-white-50 mb-4">Repositories</h2>
+                            <p class="text-white-50">
+                                Below are brief descriptions and links to all my Github repos.    
+                            </p>
+                        <div id="projectsDisplay" class="row g-5 row-cols-1 row-cols-lg-3">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--- template for project --->
+
+        <template id="projTemplate">
+            <div class="col-md-3" id="templateBorder">
+                <div>
+                    <a id="projLink" target="_blank" rel="noreferrer noopener">
+                        <h3 id="projName" class="projectName"></h3>
+                    </a>
+                    <p class="text-white-50" id="projDescription"></p>
+                </div>
+            </div>
+        </template>
+
+        <footer class="footer bg-white small text-center text-black-50">
+            <div class="my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/mcatani7/" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://github.com/michelecatani" target="_blank" rel="noopener noreferrer" aria-label="Github"><i class="fab fa-github"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://ca.linkedin.com/in/michele-catani-294136134" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/channel/UCXF6KQUCbUh4cjcdUc2bjbQ" target="_blank" rel="noopener noreferrer" aria-label="Youtube"><i class="fab fa-youtube"></i></a>
+            </div>
+            <div class="container px-4 px-lg-5">
+                Copyright &copy; Michele Catani 2022
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/projects.js" type="text/javascript"></script>
+        <script src="js/scripts.js" type="text/javascript"></script>
+        <script src="js/videos.js" type="text/javascript"></script>
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
